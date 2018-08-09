58e73c447f04bd379a19f92dd0f125989d31b898
llcsliuhao <llcsliuhao@didichuxing.com>
2018-08-09 16:25:39 +0800

    初始

---
+++ src/cv/c5_6/Apple.java
@@ -0,0 +1,23 @@
+/**
+ * Description:
+ * <br/>????: <a href="http://www.crazyit.org">????Java????</a>
+ * <br/>Copyright (C), 2001-2016, Yeeku.H.Lee
+ * <br/>This program is protected by copyright laws.
+ * <br/>Program Name:
+ * <br/>Date:
+ * @author Yeeku.H.Lee kongyeeku@163.com
+ * @version 1.0
+ */
+
+package cv.c5_6;
+
+
+public class Apple extends Fruit
+{
+    public static void main(String[] args)
+    {
+        Apple a = new Apple();
+        a.weight = 56;
+        a.info();
+    }
+}
